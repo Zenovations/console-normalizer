9672f950eddbe38968e012ed561c4520d9538eee
katowulf <katowulf@gmail.com>
2012-12-05 08:39:43 -0700

    remove commented block

--- normalizeconsole.js
+++ normalizeconsole.js
@@ -49,27 +49,6 @@
             return fn.apply(context, Array.prototype.concat.apply(args, arguments));
          };
       };
-//      Function.prototype.bind = function (oThis) {
-//         if (typeof this !== "function") {
-//            // closest thing possible to the ECMAScript 5 internal IsCallable function
-//            throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
-//         }
-//
-//         var aArgs = Array.prototype.slice.call(arguments, 1),
-//            fToBind = this,
-//            fNOP = function () {},
-//            fBound = function () {
-//               return fToBind.apply((this.prototype && this instanceof fNOP)
-//                  ? this
-//                  : oThis || window,
-//                  aArgs.concat(Array.prototype.slice.call(arguments)));
-//            };
-//
-//         fNOP.prototype = this.prototype;
-//         fBound.prototype = new fNOP();
-//
-//         return fBound;
-//      };
    }
 
    // IE 9 won't allow us to call console.log.apply (WTF IE!) It also reports typeof(console.log) as 'object' (UNH!)
